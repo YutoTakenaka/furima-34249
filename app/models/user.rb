9da32a3ce3fa5b4760864547583e5935903a8b1d
Yuto <yuto.basuke0508@icloud.com>
2021-05-19 16:33:02 +0900

    モデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-validates :nickname ,presence: true
-validates :email , presence: true , uniqueness: true
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    validates :birthday 
+  end
 
-validates :password , presence: true ,  length: { minimum: 6 }
-PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-validates_format_of :password, with: PASSWORD_REGEX
-with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-  
-  validates :first_name
-  validates :last_name
-end
+  validates :password, presence: true, length: { minimum: 6 }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 
-with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
-  validates :first_name_kana 
-  validates :last_name_kana 
-end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
 
-validates :birthday , presence: true
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+
 end
