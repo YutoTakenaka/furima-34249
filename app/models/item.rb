85b6bd37991133d7956cd5aba66a948c6d833de3
Yuto <yuto.basuke0508@icloud.com>
2021-05-20 10:56:16 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,5 +16,14 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :status
-  
+
+
+  validates :item_name ,:discription ,:category_id ,:status_id ,
+            :delivery_change_id ,:delivery_time_id ,:prefecture_id ,
+            :price ,presence: true
+
+  validates :category_id ,:status_id ,
+            :delivery_change_id ,:delivery_time_id ,:prefecture_id ,
+            numericality: { other_than: 1 }
+
 end
