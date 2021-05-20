a2f495b050d60e8ad075585b5179e4cc5c8a208f
Yuto <yuto.basuke0508@icloud.com>
2021-05-20 10:47:40 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery_change
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery_time
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+  
 end
