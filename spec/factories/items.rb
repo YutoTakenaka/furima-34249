929c7e27ec237c3690b85f66a5f88349618b0290
Yuto <yuto.basuke0508@icloud.com>
2021-05-24 16:54:47 +0900

    単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,6 +8,11 @@ FactoryBot.define do
     delivery_time_id            { Faker::Number.between(from: 2, to: 4) }
     prefecture_id               { Faker::Number.between(from: 2, to: 48) }
     price                       { Faker::Number.between(from: 300, to: 9999999) }
+   
+    association :user
   
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/item-sample.png'), filename: 'item-sample.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Item, type: :model do
     context '商品出品ができる場合' do
 
       it '必須項目が入力されていれば出品できる' do
-        
+        expect(@item).to be_valid
       end
 
     end
@@ -18,47 +18,75 @@ RSpec.describe Item, type: :model do
     context '商品出品ができない場合' do
       
       it 'imageが空では出品できない' do
-        
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
 
       it 'item_nameが空では出品できない' do
-        
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
      
       it 'descriptionが空では出品できない' do
-        
+         @item.description = ''
+         @item.valid?
+         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
      
       it 'category_idが１だと出品できない' do
-        
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
       it 'status_idが１だと出品できない' do
-        
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
 
       it 'delivery_change_idが１だと出品できない' do
-        
+        @item.delivery_change_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery change must be other than 1")
       end
 
       it 'prefecture_idが１だと出品できない' do
-        
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
 
       it 'delivery_time_idが１だと出品できない' do
-        
+        @item.delivery_time_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time must be other than 1")
       end
 
       it 'priceが300未満では出品できない' do
-        
+        @item.price = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
 
       it 'priceが9999999より大きいと出品できない' do
-        
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
-      
+
       it 'priceが空では出品できない' do
-        
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'userと結びついていないとできない' do
+         @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
 
     end
