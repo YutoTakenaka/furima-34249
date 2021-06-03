238b240f35bb1ca3d5903a90052f3fff8d6e89f0
Yuto <yuto.basuke0508@icloud.com>
2021-06-03 10:31:36 +0900

    異常系テストの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -79,6 +79,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
+      it 'priceに数字以外が含まれている場合は出品できない' do
+        @item.price = '1234a'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
       it 'userと結びついていないとできない' do
         @item.user = nil
         @item.valid?
