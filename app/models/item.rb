3b2c9531dc00ede38581650865b0ce9ae892a691
Yuto <yuto.basuke0508@icloud.com>
2021-06-05 22:11:30 +0900

    rubocopでの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,11 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :category
-    belongs_to :delivery_change
-    belongs_to :delivery_time
-    belongs_to :prefecture
-    belongs_to :status
+  belongs_to :category
+  belongs_to :delivery_change
+  belongs_to :delivery_time
+  belongs_to :prefecture
+  belongs_to :status
 
   with_options presence: true do
     validates :image
