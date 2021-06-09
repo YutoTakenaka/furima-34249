16e36d6f7da6c755c59cb688d2c0790265c5323b
Yuto <yuto.basuke0508@icloud.com>
2021-06-09 12:01:37 +0900

    URLを直接入力しても編集画面に遷移しない設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!,except: [:show, :index]
+  before_action :move_to_index, only: [:edit]
+
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,9 +25,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to item_path(item.id),method: :get
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id),method: :get
     else
       render :edit
     end
@@ -41,4 +43,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :item_name, :description, :price, :delivery_change_id, :delivery_time_id, :status_id,
                                  :prefecture_id, :category_id).merge(user_id: current_user.id)
   end
+end
+
+def move_to_index
+  item = Item.find(params[:id])
+     unless current_user.id == item.user_id
+      redirect_to action: :index
+     end
 end
