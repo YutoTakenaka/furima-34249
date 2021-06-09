6128af8a4063fd210b949b117958f78a7ec74a10
Yuto <yuto.basuke0508@icloud.com>
2021-06-09 13:38:07 +0900

    rubocopの使用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,except: [:show, :index]
-  before_action :move_to_index, only: [:edit ,:update]
-  before_action :set_item, only: [:show, :edit ,:update]
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :move_to_index, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to item_path(@item.id),method: :get
+      redirect_to item_path(@item.id), method: :get
     else
       render :edit
     end
@@ -44,9 +44,7 @@ end
 
 def move_to_index
   item = Item.find(params[:id])
-     unless current_user.id == item.user_id
-      redirect_to action: :index
-     end
+  redirect_to action: :index unless current_user.id == item.user_id
 end
 
 def set_item
