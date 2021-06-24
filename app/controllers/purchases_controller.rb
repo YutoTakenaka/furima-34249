a847f7f86e2777edcc6eb5380c9498e4c535d9a0
Yuto <yuto.basuke0508@icloud.com>
2021-06-25 08:29:41 +0900

    createアクションの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,4 +3,20 @@ class PurchasesController < ApplicationController
     @purchase_address = PurchaseAddress.new
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :tel).merge(user_id: current_user.id)
+  end
 end
