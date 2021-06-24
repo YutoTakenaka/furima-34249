7656c91b55441f05e80d6e17baea12760f1e887f
Yuto <yuto.basuke0508@icloud.com>
2021-06-24 12:05:54 +0900

    バリデーションの追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -3,13 +3,19 @@ class PurchaseAddress
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :tel, :purchase_id, :user_id, :item_id 
 
   with_options presence: true do
-    validates :postal_code 
-    validates :prefecture_id
+    validates :postal_code ,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
     validates :tel,format: {with: ^0\d{9,10}$ }
+    validates :user_id
+    validates :purchase_id
+    validates :item_id
+  end
+  
+  validates :prefecture_id,numericality: {other_than: 0, message: "can't be blank"}
+ 
+  def save
+    
   end
 
- 
-  
 end
